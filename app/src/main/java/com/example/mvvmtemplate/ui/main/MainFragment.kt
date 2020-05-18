2bf0c93fd229fb3cfb216c320cbf0f1ca104ac38
Kazuki Yamashiro <kyamashiro73@gmail.com>
2020-05-19 07:49:12 +0900

    Navigation Componentを使用できるよう設定した

--- app/src/main/java/com/example/mvvmtemplate/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvmtemplate/ui/main/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.example.mvvmtemplate.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.mvvmtemplate.R
 
 class MainFragment : Fragment() {
@@ -15,6 +18,7 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,4 +33,11 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.main_fragment_button).setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+    }
 }
